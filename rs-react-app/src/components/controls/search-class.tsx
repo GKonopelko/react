b760209e42c8a5525fa4fafa540a308ec7eb6933
Grigori Konopelko <grigori.konopelko@gmail.com>
2025-07-09 18:37:12 +0300

    feat: add class methods

--- rs-react-app/src/components/controls/search-class.tsx
+++ rs-react-app/src/components/controls/search-class.tsx
@@ -1,18 +1,49 @@
-import * as React from 'react';
+import React from 'react';
 import styles from './styles.module.css';
 
 export class Search extends React.Component {
+  state = {
+    queryContent: '',
+  };
+
+  writeToLS = (query: string) => {
+    localStorage.setItem('queryContent', query);
+  };
+
+  readFromLS = (): string => {
+    return localStorage.getItem('queryContent') || '';
+  };
+
+  componentDidMount(): void {
+    const savedQuery = this.readFromLS();
+    this.setState({ queryContent: savedQuery });
+  }
+
+  handleFormInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const targetValue = event.target.value;
+    this.setState({ queryContent: targetValue });
+    this.writeToLS(targetValue);
+  };
+
+  handleFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(event, 'queryContent  ', this.state.queryContent);
+  };
+
   render() {
     return (
       <form
         className={styles['search-component']}
-        onSubmit={(event) => {
-          event.preventDefault();
-          console.log(event);
-        }}
+        onSubmit={this.handleFormSubmit}
       >
-        <input type="text" placeholder="Search" />
-        <button>Search</button>
+        <input
+          type="text"
+          id="id{value}"
+          placeholder="Search"
+          value={this.state.queryContent}
+          onChange={this.handleFormInput}
+        />
+        <button type="submit">Search</button>
       </form>
     );
   }
