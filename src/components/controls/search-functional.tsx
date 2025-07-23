6bca2a1cb396c15a74767155b0876661df4a75a5
Grigori Konopelko <grigori.konopelko@gmail.com>
2025-07-23 13:22:18 +0300

    refactor: add search function

---
+++ src/components/controls/search-functional.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState, type ChangeEvent, type FormEvent } from 'react';
+import styles from './styles.module.css';
+
+interface SearchProps {
+  onSearch: (query: string) => void;
+}
+
+export const Search = ({ onSearch }: SearchProps) => {
+  const [queryContent, setQueryContent] = useState<string>(
+    localStorage.getItem('poke-monReactQueryContent') || ''
+  );
+  console.log(queryContent);
+
+  useEffect(() => {
+    const savedQuery = localStorage.getItem('poke-monReactQueryContent');
+    if (savedQuery) {
+      setQueryContent(savedQuery);
+    }
+    console.log(savedQuery);
+  }, []);
+
+  const handleFormInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setQueryContent(event.target.value);
+  };
+
+  const handleFormSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    const trimmedQuery = queryContent.trim();
+    localStorage.setItem('poke-monReactQueryContent', trimmedQuery);
+    onSearch(trimmedQuery);
+    console.log(trimmedQuery);
+  };
+
+  return (
+    <form className={styles['search-component']} onSubmit={handleFormSubmit}>
+      <input
+        type="text"
+        id="search-input"
+        placeholder="Enter pokemon name or id"
+        value={queryContent}
+        onChange={handleFormInput}
+      />
+      <button type="submit">Search pokemon</button>
+    </form>
+  );
+};
