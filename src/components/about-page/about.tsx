4b5599556b90f846552785e4980358a652a37f76
Grigori Konopelko <grigori.konopelko@gmail.com>
2025-07-24 13:13:37 +0300

    feat: add about page

--- src/components/about-page/about.tsx
+++ src/components/about-page/about.tsx
@@ -1,29 +1,42 @@
 import styles from './styles.module.css';
-import rssLogo from '../../assets/rss-logo.svg';
+import { Link } from 'react-router-dom';
+import { Footer } from '../footer/footer';
 
 export const About = () => {
   return (
     <div className={styles.about}>
+      <Link to="/" className={styles.link}>
+        Go back to Pokemons
+      </Link>
       <h2>About Poke-monReact</h2>
       <div className={styles.content}>
         <p>
           This application was developed by Grigori Konopelko as part of the RS
           School React course.
         </p>
-        <p>It uses the PokeAPI to fetch and display Pokémon information.</p>
-        <a
-          href="https://rs.school/react/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.link}
-        >
-          <img
-            src={rssLogo}
-            alt="RS School React Course"
-            className={styles.logo}
-          />
-          RS School React Course
-        </a>
+        <p>It uses the PokeAPI, React Router and Error Boundary.</p>
+        <p>
+          More information about the &nbsp;
+          <a
+            href="https://github.com/rolling-scopes-school/tasks/blob/master/react/modules/tasks/functional-routing.md"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles['link']}
+          >
+            task
+          </a>{' '}
+          and about the &nbsp;
+          <a
+            href="https://rs.school/courses/reactjs"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles['link']}
+          >
+            course
+          </a>
+          .
+        </p>
+        <Footer></Footer>
       </div>
     </div>
   );
