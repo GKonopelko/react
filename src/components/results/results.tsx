2efcec811e4f3bbc33ed9b1e537d6c589196cfff
Grigori Konopelko <grigori.konopelko@gmail.com>
2025-08-01 19:33:31 +0300

    refactor: styles

--- src/components/results/results.tsx
+++ src/components/results/results.tsx
@@ -43,7 +43,7 @@ export const Results = ({
   useEffect(() => {
     if (resultsContainerRef.current) {
       resultsContainerRef.current.style.overflow = 'auto';
-      resultsContainerRef.current.style.height = '100vh';
+      resultsContainerRef.current.style.height = 'auto';
       resultsContainerRef.current.scrollTo(0, 0);
     }
   }, [currentPage]);
